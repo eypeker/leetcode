db348f7a2fa7076def6bbfcc1d8cf91da033ed9a
Eyüp Eker <eyuep.eker@stud.th-luebeck.de>
2023-04-11 06:52:48 +0200

    adding a small server

---
+++ trapping_rain_water/server.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.directory = "./www/"
+Handler.extensions_map.update({
+      ".js": "application/javascript",
+});
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.serve_forever()
